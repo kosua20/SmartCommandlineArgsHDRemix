70d4bbdcd69e5381f72af8a7dbd445d28752f647
Simon Rodriguez <kosua20@gmail.com>
2025-03-06 17:40:11 +0100

    Fix GUID inconsistency.

--- SmartCmdArgs/SmartCmdArgs.Shared/CustomMonikers.cs
+++ SmartCmdArgs/SmartCmdArgs.Shared/CustomMonikers.cs
@@ -5,7 +5,7 @@ namespace SmartCmdArgs
 {
     internal class CustomMonikers
     {
-        private static readonly Guid ManifestGuid = new Guid( "25e29480-bf22-41dc-8276-bd488424d8fa" );
+        private static readonly Guid ManifestGuid = new Guid( "456afed8-eeb0-4e2d-bb64-c74a4603a623" );
 
         public static ImageMoniker FoProjectNode => new ImageMoniker { Guid = ManifestGuid, Id = 0 };
         public static ImageMoniker CopyCmdLine => new ImageMoniker { Guid = ManifestGuid, Id = 1 };
